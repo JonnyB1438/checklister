94ef3d88228221252ea60f1be976bba9b35b5d0c
JonnyB1438 <zemnicus@gmail.com>
2022-01-11 14:48:37 +0700

    created model CheckListTemplate

--- checklister/checklist/models.py
+++ checklister/checklist/models.py
@@ -3,6 +3,11 @@ from django.conf import settings
 
 # Create your models here.
 class Directory(models.Model):
+    """
+    This model stores a structure of directory, where:
+        - parent is a parent directory id
+        - owner is an user id from AUTH_USER_MODEL
+    """
     name = models.CharField(max_length=255,)
     parent = models.IntegerField(blank=True,)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
@@ -10,4 +15,15 @@ class Directory(models.Model):
     class Meta:
         ordering = ['owner', 'parent', 'name']
 
+class CheckListTemplate(models.Model):
+    """
+    This model stores checklist templates with reference to a directory, where:
+        - list is a checklist JSON dump
+        - directory is a directory id from Directory model
+    """
+    name = models.CharField(max_length=255,)
+    list = models.TextField(blank=True,)
+    directory = models.ForeignKey('Directory', on_delete=models.CASCADE,)
 
+    class Meta:
+        ordering = ['directory', 'name',]
