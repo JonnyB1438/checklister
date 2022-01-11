545a8b6dc9d9f2fa9e994086d901305fb9762547
JonnyB1438 <zemnicus@gmail.com>
2022-01-11 13:53:57 +0700

    created model Directory

--- checklister/checklist/models.py
+++ checklister/checklist/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Directory(models.Model):
+    name = models.CharField(max_length=255,)
+    parent = models.IntegerField(blank=True,)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
+
+    class Meta:
+        ordering = ['owner', 'parent', 'name']
+
+
--- checklister/checklister/settings.py
+++ checklister/checklister/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'checklist.app.ChecklistConfig'
+    # 'checklist.app.ChecklistConfig'
+    'checklist'
 ]
 
 MIDDLEWARE = [
