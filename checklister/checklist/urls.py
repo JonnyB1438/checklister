50df3024e3d6b81c1ed5f94e6e444440fcc131e6
JonnyB1438 <zemnicus@gmail.com>
2022-02-13 12:27:42 +0700

    Rewrited on CBV methods

--- checklister/checklist/urls.py
+++ checklister/checklist/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path
 from . import views
+from .views import AjaxHandlerView
 
 
 urlpatterns = [
-    path('', views.index, name='checklist'),
+    # path('', views.index, name='checklist'),
+    path('', AjaxHandlerView.as_view(), name='checklist'),
 ]
--- checklister/checklist/views.py
+++ checklister/checklist/views.py
@@ -1,40 +1,31 @@
 from django.shortcuts import render
+from django.views import View
+
 from .services import *
 
 
-# Create your views here.
-def index(request):
-    """
-    The main view of application checks an auth and type of request and sends it to the appropriate handler:
-    a built-in template engine or an ajax handler(GET or POST).
+class AjaxHandlerView(View):
+    #     """
+    #     The main view class of application checks an auth and type of request and sends it to the appropriate handler:
+    #     a built-in template engine or an ajax handler(GET or POST).
+    #     """
 
-    :param request: a request from front-end.
-    """
-    if request.user.is_authenticated:
-        print(f'User: {request.user.username}')
-        if request.is_ajax():
-            print('------------------AJAX------------------')
-            if request.GET:
+    def get(self, request):
+        if request.user.is_authenticated:
+            if request.is_ajax():
                 return get_handler(request=request)
-            elif request.POST:
+            else:
+                root_directory = get_root_directory(owner=request.user)
+                content = get_json_directory_content(owner=request.user, directory_id=root_directory["id"])
+                return render(request, 'checklist/checklist.html', {'content': content})
+        return render(request, 'checklist/index.html')
+
+    def post(self, request):
+        if request.user.is_authenticated:
+            if request.is_ajax():
+                print('------------------AJAX------------------')
                 return post_handler(request=request)
             else:
                 return JsonResponse({'error': 'Error'}, status=500)
-        else:
-            print('------------------LOAD------------------')
-            root_directory = get_root_directory(owner=request.user)
-            content = get_json_directory_content(owner=request.user, directory_id=root_directory["id"])
-            return render(request, 'checklist/checklist.html', {'content': content})
-    return render(request, 'checklist/index.html')  # TODO change to redirection on a login page
+        return render(request, 'checklist/index.html')
 
-
-# class AjaxHandlerView(View):
-#     def get(self, request):
-#         text = request.GET.get('element_text')
-#         print()
-#         print(text)
-#         print()
-#         if request.is_ajax():
-#             t = time()
-#             return JsonResponse({'seconds':t}, status=200)
-#         return render(request, 'checklist/checklist.html')
