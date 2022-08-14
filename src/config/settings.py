4101ed6d02e338ca6b75509911f711d61785c821
JonnyB1438 <zemnicus@gmail.com>
2022-08-14 18:43:27 +0700

    added Google auth

--- src/config/settings.py
+++ src/config/settings.py
@@ -31,9 +31,7 @@ ALLOWED_HOSTS = ['checklister.site',
                  ]
 
 # Application definition
-
 INSTALLED_APPS = [
-    # 'checklist.app.ChecklistConfig'
     'apps.checklist',
     'apps.users',
 
@@ -43,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'social_django',
 ]
 
@@ -148,7 +147,22 @@ SOCIAL_AUTH_JSONFIELD_ENABLED = True
 
 SOCIAL_AUTH_VK_OAUTH2_KEY = variables.SOCIAL_AUTH_VK_OAUTH2_KEY
 SOCIAL_AUTH_VK_OAUTH2_SECRET = variables.SOCIAL_AUTH_VK_OAUTH2_SECRET
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = variables.SOCIAL_AUTH_GOOGLE_OAUTH2_KEY
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = variables.SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET
+
 # SOCIAL_AUTH_VK_OAUTH2_SCOPE = ['email', ]
+# SOCIAL_AUTH_PIPELINE = (
+#     'social_core.pipeline.social_auth.social_details',
+#     'social_core.pipeline.social_auth.social_uid',
+#     'social_core.pipeline.social_auth.social_user',
+#     'social_core.pipeline.user.get_username',
+#     'social_core.pipeline.social_auth.associate_by_email',
+#     'social_core.pipeline.user.create_user',
+#     'social_core.pipeline.social_auth.associate_user',
+#     'social_core.pipeline.social_auth.load_extra_data',
+#     'social_core.pipeline.user.user_details',
+# )
 
 AUTHENTICATION_BACKENDS = (
     'social_core.backends.vk.VKOAuth2',  # vk auth
